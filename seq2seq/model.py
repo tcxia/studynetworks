554f669b3c020c491577f808c759f0efb21487a5
tcxia <muctianciking@163.com>
2022-04-21 15:17:52 +0800

    seq2seq

---
+++ seq2seq/model.py
@@ -0,0 +1,106 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+import math
+import random
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, embed_size, hidden_size, n_layer=1, dropout=0.5) -> None:
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.embed_size = embed_size
+        self.embed = nn.Embedding(input_size, embed_size)
+        self.gru = nn.GRU(embed_size, hidden_size, n_layer, dropout=dropout, bidirectional=True)
+
+    def forward(self, src, hidden=None):
+        embedded = self.embed(src)
+        outputs, hidden = self.gru(embedded, hidden)
+        outputs = (outputs[:, :, :self.hidden_size] + outputs[:, :, self.hidden_size:])
+        return outputs, hidden
+
+    
+class Attention(nn.Module):
+    def __init__(self, hidden_size) -> None:
+        super().__init__()
+        self.hidden_size =hidden_size
+        self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+        self.v = nn.Parameter(torch.rand(hidden_size))
+        stdv = 1. / math.sqrt(self.v.size(0))
+        self.v.data.uniform_(-stdv, stdv)
+
+    def forward(self, hidden, encoder_outputs):
+        timestep = encoder_outputs.size(0)
+        h = hidden.repeat(timestep, 1, 1).transpose(0, 1)
+        encoder_outputs = encoder_outputs.transpose(0, 1)
+        attn_energies = self.score(h, encoder_outputs)
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
+
+    def score(self, hidden, encoder_outputs):
+        energy = F.relu(self.attn(torch.cat([hidden, encoder_outputs], 2)))
+        energy = energy.transpose(1, 2)
+        v = self.v.repeat(encoder_outputs.size(0), 1).unsqueeze(1)
+        energy = torch.bmm(v, energy)
+        return energy.squeeze(1)
+
+
+class Decoder(nn.Module):
+    def __init__(self, embed_size, hidden_size, output_size, n_layer=1, dropout=0.2) -> None:
+        super().__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layer = n_layer
+
+        self.embed = nn.Embedding(output_size, embed_size)
+        self.dropout = nn.Dropout(dropout, inplace=True)
+        self.attn = Attention(hidden_size)
+        self.gru = nn.GRU(hidden_size + embed_size, hidden_size, n_layer, dropout=dropout)
+
+        self.out = nn.Linear(hidden_size * 2, output_size)
+
+    def forward(self, input, last_hidden, encoder_outputs):
+        embedded = self.embed(input)
+        embedded = self.dropout(embedded)
+
+        attn_weights = self.attn(last_hidden[-1], encoder_outputs)
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
+        context = context.transpose(0, 1)
+
+        rnn_input = torch.cat([embedded, context], 2)
+        output, hidden = self.gru(rnn_input, last_hidden)
+        output = output.squeeze(0)
+        context = context.squeeze(0)
+        output = self.out(torch.cat([output, context], 1))
+        output = F.log_softmax(output, dim=1)
+        return output, hidden, attn_weights
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = src.size(1)
+        max_len = trg.size(0)
+        vocab_size = self.decoder.output_size
+        outputs = Variable(torch.zeros(max_len, batch_size, vocab_size)).cuda()
+
+        encoder_output, hidden = self.encoder(src)
+        hidden = hidden[:self.decoder.n_layer]
+        output = Variable(trg.data[0, :])
+
+        for t in range(1, max_len):
+            output, hidden, attn_weight = self.decoder(output, hidden, encoder_output)
+            outputs[t] = output
+            is_teacher = random.random() < teacher_forcing_ratio
+            top1 = output.data.max(1)[1]
+            output = Variable(trg.data[t] if is_teacher else top1).cuda()
+
+        return outputs
