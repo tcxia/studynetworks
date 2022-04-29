514e5cb39e5b5329557689a3560777dd16adb6a5
tcxia <muctianciking@163.com>
2022-04-29 15:16:32 +0800

    SimCSE

--- SimCSE/model.py
+++ SimCSE/model.py
@@ -105,18 +105,107 @@ def cl_forward(cls,
     attention_mask = attention_mask.view((-1, attention_mask.size(-1)))
     if token_type_ids is not None:
         token_type_ids = token_type_ids.view((-1, token_type_ids.size(-1)))
-    
-    outputs = encoder(
-        input_ids,
-        attention_mask=attention_mask,
-        token_type_ids=token_type_ids,
-        position_ids=position_ids,
-        head_mask=head_mask,
-        inputs_embeds=inputs_embeds,
-        output_attentions=output_attentions,
-        output_hidden_states= True if cls.model_args.pooler_type in ['avg_top2', 'avg_first_last'] else False,
-        return_dict=True
-    )
-    
+
+    outputs = encoder(input_ids,
+                      attention_mask=attention_mask,
+                      token_type_ids=token_type_ids,
+                      position_ids=position_ids,
+                      head_mask=head_mask,
+                      inputs_embeds=inputs_embeds,
+                      output_attentions=output_attentions,
+                      output_hidden_states=True if cls.model_args.pooler_type
+                      in ['avg_top2', 'avg_first_last'] else False,
+                      return_dict=True)
+
     if mlm_input_ids is not None:
-        mlm_input_ids
+        mlm_input_ids = mlm_input_ids.view((-1, mlm_input_ids.size(-1)))
+        mlm_outputs = encoder(
+            mlm_input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=True if cls.model_args.pooler_type
+            in ['avg_top2', 'avg_first_last'] else False,
+            return_dict=True,
+        )
+
+    pooler_outout = cls.pooler(attention_mask, outputs)
+    pooler_output = pooler_outout.view(
+        (batch_size, num_sent, pooler_outout.size(-1)))
+
+    if cls.pooler_type == 'cls':
+        pooler_outout = cls.mlp(pooler_outout)
+
+    z1, z2 = pooler_outout[:, 0], pooler_outout[:, 1]
+    # Hard negative
+    if num_sent == 3:
+        z3 = pooler_output[:, 2]
+
+    # Gather all embeddings if using distributed training
+    if dist.is_initialized() and cls.training:
+        # Gather hard negative
+        if num_sent >= 3:
+            z3_list = [
+                torch.zeros_like(z3) for _ in range(dist.get_world_size())
+            ]
+            dist.all_gather(tensor_list=z3_list, tensor=z3.contiguous())
+            z3_list[dist.get_rank()] = z3
+            z3 = torch.cat(z3_list, 0)
+
+        # Dummy vectors for allgather
+        z1_list = [torch.zeros_like(z1) for _ in range(dist.get_world_size())]
+        z2_list = [torch.zeros_like(z2) for _ in range(dist.get_world_size())]
+        # Allgather
+        dist.all_gather(tensor_list=z1_list, tensor=z1.contiguous())
+        dist.all_gather(tensor_list=z2_list, tensor=z2.contiguous())
+
+        # Since allgather results do not have gradients, we replace the
+        # current process's corresponding embeddings with original tensors
+        z1_list[dist.get_rank()] = z1
+        z2_list[dist.get_rank()] = z2
+        # Get full batch embeddings: (bs x N, hidden)
+        z1 = torch.cat(z1_list, 0)
+        z2 = torch.cat(z2_list, 0)
+
+    cos_sim = cls.sim(z1.unsqueeze(1), z2.unsqueeze(0))
+    # Hard negative
+    if num_sent >= 3:
+        z1_z3_cos = cls.sim(z1.unsqueeze(1), z3.unsqueeze(0))
+        cos_sim = torch.cat([cos_sim, z1_z3_cos], 1)
+
+    labels = torch.arange(cos_sim.size(0)).long().to(cls.device)
+    loss_fct = nn.CrossEntropyLoss()
+
+    # Calculate loss with hard negatives
+    if num_sent == 3:
+        # Note that weights are actually logits of weights
+        z3_weight = cls.model_args.hard_negative_weight
+        weights = torch.tensor(
+            [[0.0] * (cos_sim.size(-1) - z1_z3_cos.size(-1)) + [0.0] * i +
+             [z3_weight] + [0.0] * (z1_z3_cos.size(-1) - i - 1)
+             for i in range(z1_z3_cos.size(-1))]).to(cls.device)
+        cos_sim = cos_sim + weights
+
+    loss = loss_fct(cos_sim, labels)
+
+    # Calculate loss for MLM
+    if mlm_outputs is not None and mlm_labels is not None:
+        mlm_labels = mlm_labels.view(-1, mlm_labels.size(-1))
+        prediction_scores = cls.lm_head(mlm_outputs.last_hidden_state)
+        masked_lm_loss = loss_fct(
+            prediction_scores.view(-1, cls.config.vocab_size),
+            mlm_labels.view(-1))
+        loss = loss + cls.model_args.mlm_weight * masked_lm_loss
+
+    if not return_dict:
+        output = (cos_sim, ) + outputs[2:]
+        return ((loss, ) + output) if loss is not None else output
+    return SequenceClassifierOutput(
+        loss=loss,
+        logits=cos_sim,
+        hidden_states=outputs.hidden_states,
+        attentions=outputs.attentions,
+    )
