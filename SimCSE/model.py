4097364571e00bc955872a20f02e14435a8f8176
tcxia <muctianciking@163.com>
2022-05-06 13:33:06 +0800

    SimCSE

--- SimCSE/model.py
+++ SimCSE/model.py
@@ -1,10 +1,13 @@
+from cProfile import label
 import torch
 import torch.nn as nn
 
 import torch.distributed as dist
 
 import transformers
-from transformers.modeling_outputs import SequenceClassifierOutput
+from transformers.modeling_outputs import SequenceClassifierOutput, BaseModelOutputWithPoolingAndCrossAttentions
+from transformers.models.bert.modeling_bert import BertPreTrainedModel, BertModel, BertLMPredictionHead
+from transformers.models.roberta.modeling_roberta import RobertaPreTrainedModel, RobertaModel, RobertaLMHead
 
 
 class MLPLayer(nn.Module):
@@ -212,3 +215,157 @@ def cl_forward(cls,
         hidden_states=outputs.hidden_states,
         attentions=outputs.attentions,
     )
+
+
+def sentemb_forward(cls,
+                    encoder,
+                    input_ids=None,
+                    attention_mask=None,
+                    token_type_ids=None,
+                    position_ids=None,
+                    head_mask=None,
+                    inputs_embeds=None,
+                    labels=None,
+                    output_attentions=None,
+                    output_hidden_states=None,
+                    return_dict=None):
+    return_dict = return_dict if return_dict is not None else cls.config.use_return_dict
+
+    outputs = encoder(input_ids,
+                      attention_mask=attention_mask,
+                      token_type_ids=token_type_ids,
+                      position_ids=position_ids,
+                      head_mask=head_mask,
+                      inputs_embeds=inputs_embeds,
+                      output_attentions=output_attentions,
+                      output_hidden_states=True if cls.pooler_type
+                      in ['avg_top2', 'avg_first_last'] else False,
+                      return_dict=True)
+    pooler_output = cls.pooler(attention_mask, outputs)
+    if cls.pooler_type == 'cls' and not cls.model_args.mlp_only_train:
+        pooler_output = cls.mlp(pooler_output)
+
+    if not return_dict:
+        return (outputs[0], pooler_output) + outputs[2:]
+
+    return BaseModelOutputWithPoolingAndCrossAttentions(
+        pooler_output=pooler_output,
+        last_hidden_state=outputs.last_hidden_state,
+        hidden_states=outputs.hidden_states,
+    )
+
+
+class BertForCL(BertPreTrainedModel):
+    _keys_to_ignore_on_load_missing = [r'position_ids']
+
+    def __init__(self, config, *model_args, **model_kargs):
+        super().__init__(config)
+        self.model_args = model_kargs['model_args']
+        self.bert = BertModel(config, add_pooling_layer=False)
+
+        if self.model_args.do_mlm:
+            self.lm_head = BertLMPredictionHead(config)
+
+        cl_init(self, config)
+
+    def forward(self,
+                input_ids=None,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None,
+                head_mask=None,
+                input_embeds=None,
+                labels=None,
+                output_attentions=None,
+                output_hidden_states=None,
+                return_dict=None,
+                sent_emb=False,
+                mlm_input_ids=None,
+                mlm_labels=None):
+        if sent_emb:
+            return sentemb_forward(self,
+                                   self.bert,
+                                   input_ids=input_ids,
+                                   attention_mask=attention_mask,
+                                   token_type_ids=token_type_ids,
+                                   position_ids=position_ids,
+                                   head_mask=head_mask,
+                                   input_embeds=input_embeds,
+                                   labels=labels,
+                                   output_attentions=output_attentions,
+                                   output_hidden_states=output_hidden_states,
+                                   return_dict=return_dict)
+
+        else:
+            return cl_forward(self,
+                              self.bert,
+                              input_ids=input_ids,
+                              attention_mask=attention_mask,
+                              token_type_ids=token_type_ids,
+                              position_ids=position_ids,
+                              head_mask=head_mask,
+                              input_embeds=input_embeds,
+                              labels=labels,
+                              output_attentions=output_attentions,
+                              output_hidden_states=output_hidden_states,
+                              return_dict=return_dict,
+                              mlm_input_ids=mlm_input_ids,
+                              mlm_labels=mlm_labels)
+
+
+class RobertaForCL(RobertaPreTrainedModel):
+    __keys_to_ignore_on_load_missing = [r'position_ids']
+
+    def __init__(self, config, *model_args, **model_kargs):
+        super().__init__(config)
+        self.model_args = model_kargs['model_args']
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
+
+        if self.model_args.do_mlm:
+            self.lm_head = RobertaLMHead(config)
+
+        cl_init(self, config)
+
+    def forward(self,
+                input_ids=None,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None,
+                head_mask=None,
+                input_embeds=None,
+                labels=None,
+                output_attentions=None,
+                output_hidden_states=None,
+                return_dict=None,
+                sent_emb=False,
+                mlm_input_ids=None,
+                mlm_labels=None):
+        if sent_emb:
+            return sentemb_forward(self,
+                                   self.roberta,
+                                   input_ids=input_ids,
+                                   attention_mask=attention_mask,
+                                   token_type_ids=token_type_ids,
+                                   position_ids=position_ids,
+                                   head_mask=head_mask,
+                                   input_embeds=input_embeds,
+                                   labels=labels,
+                                   output_attentions=output_attentions,
+                                   output_hidden_states=output_hidden_states,
+                                   return_dict=return_dict)
+
+        else:
+            return cl_forward(self,
+                              self.roberta,
+                              input_ids=input_ids,
+                              attention_mask=attention_mask,
+                              token_type_ids=token_type_ids,
+                              position_ids=position_ids,
+                              head_mask=head_mask,
+                              input_embeds=input_embeds,
+                              labels=labels,
+                              output_attentions=output_attentions,
+                              output_hidden_states=output_hidden_states,
+                              return_dict=return_dict,
+                              mlm_input_ids=mlm_input_ids,
+                              mlm_labels=mlm_labels)
